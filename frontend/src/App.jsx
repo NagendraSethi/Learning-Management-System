7e245f32461916126d24d227e14615a9e42ce4d9
sethi.sethi <sethi.sethi@kraftheinz.com>
2024-08-03 00:30:27 +0530

    redux setup with toolkit done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import { useEffect } from 'react'
 import './App.css'
+import toast from 'react-hot-toast'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  useEffect(()=>{
+    toast.error("hello")
+  })
 
   return (
     <>
