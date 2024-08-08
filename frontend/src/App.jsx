08e5227f025c7bd36512e423f7f3c632cb6b7e1a
sethi.sethi <sethi.sethi@kraftheinz.com>
2024-08-08 19:18:34 +0530

    Added about us page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,16 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Home from './pages/Home'
+import Aboutus from './pages/Aboutus'
 
 
 function App() {
-  
   return (
-   <Home/>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/about" element={<Aboutus/>}/>
+    </Routes>
+   
   )
 }
 
